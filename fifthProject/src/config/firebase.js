7022010b529c1b09119b4111833f94036981ab35
Kishan <kissanshaa@gmail.com>
2024-05-31 22:35:52 +0530

    firebase data store

---
+++ fifthProject/src/config/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDxUVs1Zann61I1FzU72_9jVjSiwFvSIuY",
+  authDomain: "vite-contact-ca220.firebaseapp.com",
+  projectId: "vite-contact-ca220",
+  storageBucket: "vite-contact-ca220.appspot.com",
+  messagingSenderId: "327269397520",
+  appId: "1:327269397520:web:51b197c133228d9d4ec695"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
