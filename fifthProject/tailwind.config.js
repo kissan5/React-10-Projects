f7bf962ca324284a0423c884f60f7451118821d5
Kishan <kissanshaa@gmail.com>
2024-06-08 15:26:00 +0530

    tailwind css

--- fifthProject/tailwind.config.js
+++ fifthProject/tailwind.config.js
@@ -1,18 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        gray:"#5A5959",
-        yellow:"#FFEAAE",
-       " dark-yellow":"#FCCA3F",
-        orange:"#F6820C",
-
-      }
+        gray: "#1C1C1D",
+        blue: "#0466c8",
+        "dark-yellow": "",
+        orange: "",
+      },
     },
   },
   plugins: [],
-}
+};
