6b7b6442bb660bd95c729149728285f38c77c8eb
Kishan <kissanshaa@gmail.com>
2024-06-08 15:50:02 +0530

    new

--- fifthProject/src/App.jsx
+++ fifthProject/src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import { FiSearch } from "react-icons/fi";
+import { AiFillPlusCircle } from "react-icons/ai";
+import {IoIosContact } from "react-icons/io"
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./config/firebase";
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    const getContacts = async () => {
+      try {
+        const contactsRef = collection(db, "contacts");
+        const contactsSnapshot = await getDocs(contactsRef);
+        const contactLists = contactsSnapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        });
+        setContacts(contactLists);
+      } catch (error) {}
+    };
+    getContacts();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="mx-auto max-w-[370px] px-4">
+      <Navbar />
+      <div className="flex gap-2">
+        <div className="relative flex flex-grow items-center">
+          <FiSearch className="ml-1 absolute text-white text-2xl" />
+          <input
+            type="text"
+            className="h-10 flex-grow rounded-md border border-white bg-transparent text-white pl-9"
+          />
+        </div>
+        <div>
+          <AiFillPlusCircle className="text-5xl cursor-pointer text-white " />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+<div>
+ { contacts.map((contact) => (
+  <div key={contact.id}>
+    <IoIosContact />
+    <div className="text-white">
+      <h2>{contact.name}</h2>
+      <p>{contact.email}</p>
+    </div>
+  </div>
+ )  )}
+</div>
+
+    </div>
+  );
+};
+
+export default App;
