d405c5c24e7f72d890358ed9c8d40f433d5e30b7
Kishan <kissanshaa@gmail.com>
2024-06-09 14:57:53 +0530

    modal created for add contact

--- fifthProject/src/App.jsx
+++ fifthProject/src/App.jsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import { FiSearch } from "react-icons/fi";
+import { AiFillPlusCircle } from "react-icons/ai";
+import { IoIosContact } from "react-icons/io";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./config/firebase";
+import AddAndUpdateContact from "./components/AddAndUpdateContact";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [isOpen, setOpen] = useState([]);
+
+  useEffect(() => {
+    const getContacts = async () => {
+      try {
+        const contactsRef = collection(db, "contacts");
+        const contactsSnapshot = await getDocs(contactsRef);
+        const contactLists = contactsSnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setContacts(contactLists);
+      } catch (error) {
+        console.error("Error fetching contacts: ", error);
+      }
+    };
+    getContacts();
+  }, []);
+
+  const onOpen = () => {
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <div className="mx-auto max-w-[370px] px-4">
+        <Navbar />
+        <div className="flex gap-2">
+          <div className="relative flex flex-grow items-center">
+            <FiSearch className="ml-1 absolute text-white text-2xl" />
+            <input
+              type="text"
+              className="h-10 flex-grow rounded-md border border-white bg-transparent text-white pl-9"
+            />
+          </div>
+          <div>
+            <AiFillPlusCircle
+              onClick={onOpen}
+              className="text-5xl cursor-pointer text-white"
+            />
+          </div>
+        </div>
 
-export default App
+        <div>
+          {contacts.map((contact) => (
+            <div
+              key={contact.id}
+              className="flex items-center border-b border-gray-300 py-2"
+            >
+              <IoIosContact className="text-white text-5xl mr-2" />
+              <div className="text-white">
+                <h2>{contact.name}</h2>
+                <p>{contact.email}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <AddAndUpdateContact isOpen={isOpen} onClose={onClose}/>
+    </>
+  );
+};
+
+export default App;
--- fifthProject/tailwind.config.js
+++ fifthProject/tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: "#1C1C1D",
+        blue: "#0466c8",
+        "dark-yellow": "",
+        orange: "",
+      },
+    },
   },
   plugins: [],
-}
+};
