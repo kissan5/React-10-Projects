2e8cf5093029e5a20fa9659e8ba8e6a8c16735d9
Kishan <kissanshaa@gmail.com>
2024-06-25 23:40:30 +0530

    fontSource

--- sixthProject/src/main.jsx
+++ sixthProject/src/main.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-import { ChakraProvider, theme } from "@chakra-ui/react"
-import "@fontsource/ubuntu"; 
+import { ChakraProvider, theme } from "@chakra-ui/react";
+import "@fontsource/ubuntu";
+import "@fontsource/ubuntu/400.css";
+import "@fontsource/ubuntu/500.css";
+import "@fontsource/ubuntu/700.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}/>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
