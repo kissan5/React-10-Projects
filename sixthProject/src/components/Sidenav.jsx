b2b9086c019d890cf168f5dd6564bd93458bd808
Kishan <kissanshaa@gmail.com>
2024-06-25 23:41:58 +0530

    sidenav

--- sixthProject/src/components/Sidenav.jsx
+++ sixthProject/src/components/Sidenav.jsx
@@ -19,48 +19,51 @@ const Sidenav = () => {
   ];
 
   return (
-    <Stack boxShadow="sm" maxW="16rem" h="100vh">
-      <Heading
-        textAlign="center"
-        fontWeight="bold"
-        fontSize="20px"
-        as="h1"
-        pt="3.5rem"
-      >
-        @DOSOMECODING
-      </Heading>
-      <Box mt="6" mx="3">
-        {navLinks.map((nav) => (
-          <HStack
-            borderRadius="10px"
-            key={nav.text}
-            py="3"
-            px="4"
-            _hover={{ bg: "#f3f3f7", color: "#171717", cursor: "pointer" }}
-            color="#797E82"
-          >
-            <Icon as={nav.icon} />
-            <Text fontSize="14px" fontWeight="medium">
-              {nav.text}
-            </Text>
-          </HStack>
-        ))}
+    <Stack justify="space-between" boxShadow="lg" w="16rem" h="100vh">
+      <Box>
+        <Heading
+          textAlign="center"
+          fontWeight="bold"
+          fontSize="20px"
+          as="h1"
+          pt="3.5rem"
+        >
+          @DOSOMECODING
+        </Heading>
+
+        <Box mt="6" mx="3" mb="6">
+          {navLinks.map((nav) => (
+            <HStack
+              borderRadius="10px"
+              key={nav.text}
+              px="4"
+              py="3"
+              _hover={{ bg: "#f3f3f7", color: "#171717", cursor: "pointer" }}
+              color="#797E82"
+            >
+              <Icon as={nav.icon} />
+              <Text fontSize="14px" fontWeight="medium">
+                {nav.text}
+              </Text>
+            </HStack>
+          ))}
+        </Box>
       </Box>
 
-      <HStack
-            borderRadius="10px"
-            key={nav.text}
-            py="3"
-            px="4"
-            _hover={{ bg: "#f3f3f7", color: "#171717", cursor: "pointer" }}
-            color="#797E82"
-          >
-            <Icon as={BiSupport} />
-            <Text fontSize="14px" fontWeight="medium">
-              Support
-            </Text>
-          </HStack>
-
+      <Box mx="3" mb="6">
+        <HStack
+          borderRadius="10px"
+          py="3"
+          px="4"
+          _hover={{ bg: "#f3f3f7", color: "#171717", cursor: "pointer" }}
+          color="#797E82"
+        >
+          <Icon as={BiSupport} />
+          <Text fontSize="14px" fontWeight="medium">
+            Support
+          </Text>
+        </HStack>
+      </Box>
     </Stack>
   );
 };
