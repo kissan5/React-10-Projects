518bbbbfacb22df39c77542262e79d90143711b5
Kishan <kissanshaa@gmail.com>
2024-06-25 23:42:28 +0530

    dashboard

---
+++ sixthProject/src/components/TopNav.jsx
@@ -0,0 +1,37 @@
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  HStack,
+  Heading,
+  Icon,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react";
+import React from "react";
+import { FaUserTie } from "react-icons/fa";
+
+const TopNav = () => {
+  return (
+    <Box> 
+      <HStack maxW="1280px" h="16" justify="space-between" px="32" mx="auto">
+        <Heading fontWeight="medium" fontSize="28px">Dashboard</Heading>
+        <Menu>
+          <MenuButton>
+            <Icon as={FaUserTie} fontSize="24px"/>
+          </MenuButton>
+          <MenuList>
+            <MenuItem>Logout</MenuItem>
+            <MenuItem>Support</MenuItem>
+            
+          </MenuList>
+        </Menu>
+      </HStack>
+    </Box>
+  );
+};
+
+export default TopNav;
--- sixthProject/src/pages/Dashboard/Dashboard.jsx
+++ sixthProject/src/pages/Dashboard/Dashboard.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import Sidenav from '../../components/Sidenav'
+import React from "react";
+import Sidenav from "../../components/Sidenav";
+import TopNav from "../../components/TopNav";
+import { Box, Flex } from "@chakra-ui/react";
 
 const Dashboard = () => {
   return (
     <div>
-      <Sidenav/>
+      <Flex>
+        <Sidenav />
+        <Box flexGrow={1}>
+          <TopNav />
+        </Box>
+      </Flex>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
