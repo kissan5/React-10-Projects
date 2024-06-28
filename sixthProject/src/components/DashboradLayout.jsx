3feb0208ec11da82d9c59356f9fa6f4075783546
Kishan <kissanshaa@gmail.com>
2024-06-28 23:16:12 +0530

    sideDrawer

--- sixthProject/src/components/DashboradLayout.jsx
+++ sixthProject/src/components/DashboradLayout.jsx
@@ -1,18 +1,24 @@
-import { Box, Container, Flex } from '@chakra-ui/react'
-import React, { Children } from 'react'
-import Sidenav from './Sidenav'
-import TopNav from './TopNav'
+import { Box, Container, Flex, useDisclosure } from "@chakra-ui/react";
+import React from "react";
+import Sidenav from "./Sidenav";
+import TopNav from "./TopNav";
+import SideDrawer from "./SideDrawer";
 
-const DashboradLayout = ({title, children}) => {
+const DashboardLayout = ({ title, children }) => {
+  const { isOpen, onClose, onOpen } = useDisclosure();
+
   return (
     <Flex>
-        <Sidenav />
-        <Box flexGrow={1}>
-          <TopNav title={title}/>
-          <Container maxW="1280px" {...children}/>
-        </Box>
-      </Flex>
-  )
-}
+      <Sidenav />
+      <SideDrawer isOpen={isOpen} onClose={onClose} />
+      <Box flexGrow={1}>
+        <TopNav title={title} onOpen={onOpen} />
+        <Container maxW="1280px" bg="red">
+          {children}
+        </Container>
+      </Box>
+    </Flex>
+  );
+};
 
-export default DashboradLayout
+export default DashboardLayout;
---
+++ sixthProject/src/components/SideDrawer.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+} from "@chakra-ui/react";
+
+const SideDrawer = ({ isOpen, onClose, btnRef }) => {
+  return (
+    <>
+      <Drawer
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Create your account</DrawerHeader>
+
+          <DrawerBody>btnRef</DrawerBody>
+
+          <DrawerFooter>
+            <Button variant="outline" mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme="blue">Save</Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
+
+export default SideDrawer;
