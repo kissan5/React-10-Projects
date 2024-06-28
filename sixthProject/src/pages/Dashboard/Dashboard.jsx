ea120878170aa894f4e06e4e687f68ce3f890acd
Kishan <kissanshaa@gmail.com>
2024-06-28 23:20:58 +0530

    dashboard

--- sixthProject/src/pages/Dashboard/Dashboard.jsx
+++ sixthProject/src/pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,7 @@
 import React from "react";
-import Sidenav from "../../components/Sidenav";
-import TopNav from "../../components/TopNav";
-import { Box, Flex } from "@chakra-ui/react";
 import DashboardLayout from "../../components/DashboradLayout";
 
-const Dashboard = ({}) => {
+const Dashboard = () => {
   return (
     <DashboardLayout title="Dashboard">
       <div>Hello</div>
