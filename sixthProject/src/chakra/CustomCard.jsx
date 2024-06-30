4111c0a08f04b7f4e47ec482ed7aa1e3e21733ce
Kishan <kissanshaa@gmail.com>
2024-06-30 19:13:37 +0530

    CustomCard

---
+++ sixthProject/src/chakra/CustomCard.jsx
@@ -0,0 +1,12 @@
+// components/CustomCard.jsx
+import { chakra } from "@chakra-ui/react";
+
+export const CustomCard = chakra("div", {
+  baseStyle: {
+    bg: "white",
+    borderRadius: "xl",
+    p: "6",
+  },
+});
+
+export default CustomCard;
