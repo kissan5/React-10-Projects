c39fc02ad57555a4b357a5789fe8285188b74bb1
Kishan <kissanshaa@gmail.com>
2024-06-30 23:19:38 +0530

    price section

--- sixthProject/src/pages/Dashboard/components/PriceSection.jsx
+++ sixthProject/src/pages/Dashboard/components/PriceSection.jsx
@@ -1,4 +1,17 @@
-import { HStack, Stack, Text, Tag, Icon, Button, Flex } from "@chakra-ui/react";
+import {
+  HStack,
+  Stack,
+  Text,
+  Tag,
+  Icon,
+  Button,
+  Flex,
+  Tabs,
+  Tab,
+  TabList,
+  TabPanels,
+  TabPanel,
+} from "@chakra-ui/react";
 import React from "react";
 import { CustomCard } from "../../../chakra/CustomCard";
 import { BsArrowUpRight } from "react-icons/bs";
@@ -8,8 +21,11 @@ import {
   AiOutlineArrowUp,
   AiOutlineMinusCircle,
 } from "react-icons/ai";
+import { Image } from "@chakra-ui/react";
 
 const PriceSection = () => {
+  const timeStamps = ["7:15 PM", "7:55 PM", "8:15 PM", "9:15 PM"];
+
   return (
     <CustomCard>
       <Flex justify="space-between" align="start">
@@ -40,6 +56,38 @@ const PriceSection = () => {
           <Button leftIcon={<Icon as={AiOutlineMinusCircle} />}>Sell</Button>
         </HStack>
       </Flex>
+      <Tabs variant="soft-rounded">
+        <Flex justify="end">
+          <TabList bg="black.5" p="3px">
+            {["1H", "1D", "1W", "1M"].map((tab) => (
+              <Tab
+                _selected={{ bg: "white" }}
+                key={tab}
+                fontSize="sm"
+                p="6px"
+                borderRadius="4"
+              >
+                {tab}
+              </Tab>
+            ))}
+          </TabList>
+        </Flex>
+        <TabPanels>
+          <TabPanel>
+            <Image w="100%" src="public\graph.svg" mt="48px" />
+            <HStack justify="space-between">
+              {timeStamps.map((timestamp) => (
+                <Text key={timestamp} fontSize="sm" color="black.80">
+                  {timestamp}
+                </Text>
+              ))}
+            </HStack>
+          </TabPanel>
+          <TabPanel>
+            <p>two!</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </CustomCard>
   );
 };
