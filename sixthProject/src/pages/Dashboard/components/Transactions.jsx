e89461114fd17167e80ee80bc6514bb92493e096
Kishan <kissanshaa@gmail.com>
2024-07-02 23:24:20 +0530

    Grid added

--- sixthProject/src/pages/Dashboard/components/Transactions.jsx
+++ sixthProject/src/pages/Dashboard/components/Transactions.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { Fragment } from "react";
 import CustomCard from "../../../chakra/CustomCard";
-import { Box, Flex, Grid, Icon, Stack, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Grid,
+  Icon,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 import { BsCurrencyRupee } from "react-icons/bs";
 import { FaBtc } from "react-icons/fa";
 
@@ -30,33 +39,39 @@ const Transactions = () => {
   ];
 
   return (
-    <CustomCard>
+    <CustomCard h="full">
       <Text mb="6" fontSize="sm" color="black.80">
         Recent Transactions
       </Text>
-      <Stack>
-        {transactions.map((transaction) => (
-          <Flex key={transaction.id} gap="4" w="full">
-            <Grid
-              placeItems="center"
-              bg="black.5"
-              boxSize={10}
-              borderRadius="full"
-            >
-              <Icon as={transaction.icon} />
-            </Grid>
-            <Flex justify="space-between" w="full">
-              <Stack spacing={0}>
-                <Text textStyle="h6">{transaction.text}</Text>
-                <Text fontSize="sm" color="black.40">
-                  {transaction.timestamp}
-                </Text>
-              </Stack>
-              <Text textStyle="h6">{transaction.amount}</Text>
+      <Stack spacing={4}>
+        {transactions.map((transaction, i) => (
+          <Fragment key={transaction.id}>
+            {i != 0 && <Divider />}
+            <Flex gap="4" w="full">
+              <Grid
+                placeItems="center"
+                bg="black.5"
+                boxSize={10}
+                borderRadius="full"
+              >
+                <Icon as={transaction.icon} />
+              </Grid>
+              <Flex justify="space-between" w="full">
+                <Stack spacing={0}>
+                  <Text textStyle="h6">{transaction.text}</Text>
+                  <Text fontSize="sm" color="black.40">
+                    {transaction.timestamp}
+                  </Text>
+                </Stack>
+                <Text textStyle="h6">{transaction.amount}</Text>
+              </Flex>
             </Flex>
-          </Flex>
+          </Fragment>
         ))}
       </Stack>
+      <Button w="full" colorScheme="gray" mt="6">
+        View All
+      </Button>
     </CustomCard>
   );
 };
