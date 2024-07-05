6955992f013bc2c8d15b84158d8b4f36501dafff
Kishan <kissanshaa@gmail.com>
2024-07-05 22:41:35 +0530

    react router

--- sixthProject/src/App.jsx
+++ sixthProject/src/App.jsx
@@ -1,10 +1,28 @@
-import Dashboard from "./pages/Dashboard/Dashboard"
+import { createBrowserRouter ,RouterProvider } from "react-router-dom"
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Transactions from "./pages/Dashboard/components/Transactions";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />
+  },
+  {
+    path: "/transactions",
+    element: <Transactions />
+  },
+  {
+    path: "/support",
+    element: <Support />
+  },
+]);
 
 
 function App() {
   
   return (
     <>
+    <RouterProvider router={router} />
       <Dashboard/>
     </>
   )
