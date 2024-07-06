96d921c9ba5dd61adb25b5266ad43ec5b0254a35
Kishan <kissanshaa@gmail.com>
2024-07-06 16:50:32 +0530

    tableData made

--- sixthProject/src/pages/Transaction/components/TransactionTable.jsx
+++ sixthProject/src/pages/Transaction/components/TransactionTable.jsx
@@ -1,71 +1,148 @@
-import React from 'react';
+import React from "react";
 import {
-    Table,
-    Thead,
-    Tbody,
-    Tfoot,
-    Tr,
-    Th,
-    Td,
-    TableCaption,
-    TableContainer,
-  } from '@chakra-ui/react'
-
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+  Stack,
+  Text,
+  Tag,
+} from "@chakra-ui/react";
 const TransactionTable = () => {
-const tableData = [
+  const tableData = [
     {
-        id:"HD82NA2H",
-        date:"",
-        time:"",    
-        type:{
-            name:"",
-            tag:"",
-        },
-        amount:"",
-        status:"",
-    }
-]
+      id: "HD82NA2H",
+      date: "2024-07-06",
+      time: "4:11 PM",
+      type: {
+        name: "INR Deposit",
+        tag: "E-Transfer",
+      },
+      amount: "+ ₹81,123.10",
+      status: "pending",
+    },
+    {
+      id: "HD82NA4H",
+      date: "2023-06-18",
+      time: "07:00 AM",
+      type: {
+        name: "INR Widthdraw",
+        tag: "Wire Transfer",
+      },
+      amount: "-₹55,123",
+      status: "processing",
+    },
+    {
+      id: "HD82NA5H",
+      date: "2023-06-18",
+      time: "07:00 AM",
+      type: {
+        name: "Buy",
+        tag: "BTC",
+      },
+      amount: "12.0554484 BTC",
+      status: "cancelled",
+    },
+    {
+      id: "HD82NA6H",
+      date: "2023-06-18",
+      time: "07:00 AM",
+      type: {
+        name: "Sell",
+        tag: "BTC",
+      },
+      amount: "-2.0554484 BTC",
+      status: "completed",
+    },
+    {
+      id: "HD82NA7H",
+      date: "2023-06-20",
+      time: "07:00 AM",
+      type: {
+        name: "BTC Deposit",
+      },
+      amount: "+15.5000000",
+      status: "pending",
+    },
+    {
+      id: "HD82NA8H",
+      date: "2023-06-18",
+      time: "07:00 AM",
+      type: {
+        name: "BTC Widthdraw",
+      },
+      amount: "-5.05555544",
+      status: "completed",
+    },
+  ];
+
+  const statusColor = {
+    pending: "#797E82",
+    processing: "#F5A50B",
+    completed: "#059669",
+    cancelled: "#DC2626",
+  };
 
   return (
     <TableContainer>
-  <Table variant='simple'>
-    <TableCaption>Imperial to metric conversion factors</TableCaption>
-    <Thead>
-      <Tr>
-        <Th>Id</Th>
-        <Th>Date & Time</Th>
-        <Th >Type</Th>
-        <Th >Amount</Th>
-        <Th >Status</Th>
-      </Tr>
-    </Thead>
-    <Tbody color="p.black">
-      <Tr>
-        <Td>inches</Td>
-        <Td>millimetres (mm)</Td>
-        <Td isNumeric>25.4</Td>
-      </Tr>
-      <Tr>
-        <Td>feet</Td>
-        <Td>centimetres (cm)</Td>
-        <Td isNumeric>30.48</Td>
-      </Tr>
-      <Tr>
-        <Td>yards</Td>
-        <Td>metres (m)</Td>
-        <Td isNumeric>0.91444</Td>
-      </Tr>
-    </Tbody>
-    <Tfoot>
-      <Tr>
-        <Th>To convert</Th>
-        <Th>into</Th>
-        <Th isNumeric>multiply by</Th>
-      </Tr>
-    </Tfoot>
-  </Table>
-</TableContainer>
-  )
-}
+      <Table variant="simple" colorScheme="gray">
+        <Thead>
+          <Tr>
+            <Th>Id</Th>
+            <Th>Date & Time</Th>
+            <Th>Type</Th>
+            <Th>Amount</Th>
+            <Th>Status</Th>
+          </Tr>
+        </Thead>
+        <Tbody color="p.black">
+          {tableData.map((data) => (
+            <Tr key={data.id}>
+              <Td>{data.id}</Td>
+              <Td>
+                <Stack spacing={0}>
+                  <Text fontSize="sm" fontWeight="medium">
+                    {data.date}
+                  </Text>
+                  <Text fontSize="sm" color="black.60">
+                    {data.time}
+                  </Text>
+                </Stack>
+              </Td>
+              <Td>
+                <Stack spacing={0}>
+                  <Text fontSize="sm" fontWeight="medium">
+                    {data.type.name}
+                  </Text>
+                  <Text fontSize="sm" color="black.60">
+                    {data.type?.tag}
+                  </Text>
+                </Stack>
+              </Td>
+              <Td fontSize="sm" fontWeight="medium">
+                {data.amount}
+              </Td>
+              <Td fontSize="sm" fontWeight="medium">
+                <Tag
+                  bg={statusColor[data.status]}
+                  color="white"
+                  borderRadius="full"
+                >
+                  {data.status}
+                </Tag>
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+        <Tfoot></Tfoot>
+      </Table>
+    </TableContainer>
+  );
+};
 
-export default TransactionTable
+export default TransactionTable;
