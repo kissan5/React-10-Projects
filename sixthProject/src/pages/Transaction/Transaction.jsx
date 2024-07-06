d2a8192bd8c78ed663fe4e7d238d579800dae2b7
Kishan <kissanshaa@gmail.com>
2024-07-06 16:49:46 +0530

    tab pannel made

--- sixthProject/src/pages/Transaction/Transaction.jsx
+++ sixthProject/src/pages/Transaction/Transaction.jsx
@@ -1,13 +1,63 @@
-import { Card } from "@chakra-ui/react";
+import { Button, Card, Flex, Icon, Tag } from "@chakra-ui/react";
 import React from "react";
 import DashboardLayout from "../../components/DashboradLayout";
+import { AiOutlineDownload } from "react-icons/ai";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import TransactionTable from "./components/TransactionTable";
 
-const Transaction = () => {
+const TransactionPage = () => {
+  const tabs = [
+    {
+      name: "All",
+      count: 349,
+    },
+    {
+      name: "Deposit",
+      count: 349,
+    },
+    {
+      name: "Withdraw",
+      count: 349,
+    },
+    {
+      name: "Trade",
+      count: 349,
+    },
+  ];
+
   return (
     <DashboardLayout>
-      <Card>Transaction Card</Card>
+      <Flex justify="end" mt="6" mb="3">
+        <Button leftIcon={<Icon as={AiOutlineDownload} />}>Export CSV</Button>
+      </Flex>
+      <Card borderRadius="1rem">
+        <Tabs>
+          <TabList py="4">
+            {tabs.map((tab) => (
+              <Tab key={tab.name} display="flex" gap="2">
+                {tab.name}
+                <Tag colorScheme="gray" borderRadius="full">
+                  {tab.count}
+                </Tag>
+              </Tab>
+            ))}
+          </TabList>
+
+          <TabPanels>
+            <TabPanel>
+              <TransactionTable />
+            </TabPanel>
+            <TabPanel>
+              <p>two!</p>
+            </TabPanel>
+            <TabPanel>
+              <p>three!</p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Card>
     </DashboardLayout>
   );
 };
 
-export default Transaction;
+export default TransactionPage;
