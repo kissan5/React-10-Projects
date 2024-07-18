a7a83132019deaf74bb893aec22427c6776e91ca
Kishan <kissanshaa@gmail.com>
2024-07-18 19:30:00 +0530

    album data with its index

---
+++ spotify-clone/src/components/DisplayHome.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Navbar from "./Navbar";
+import { albumsData } from "../assets/assets";
+import AlbumItem from "./AlbumItem";
+import { songsData } from "../assets/assets";
+import SongItem from "./SongItem";
+
+const DisplayHome = () => {
+  console.log(albumsData);
+  return (
+    <>
+      <Navbar />
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Features Charts</h1>
+        <div className="flex overflow-auto">
+          {albumsData.map((item, index) => (
+            <div key={index}>
+              <AlbumItem
+                name={item.name}
+                desc={item.desc}
+                id={item.id}
+                image={item.image}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Todays biggest hit</h1>
+        <div className="flex overflow-auto">
+          {songsData.map((item, index) => (
+            <SongItem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DisplayHome;
