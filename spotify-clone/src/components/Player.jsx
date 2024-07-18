86b26509bbe234c1b83950220f08957ec869e691
Kishan <kissanshaa@gmail.com>
2024-07-18 21:28:32 +0530

    assets icon for the player

--- spotify-clone/src/components/Player.jsx
+++ spotify-clone/src/components/Player.jsx
@@ -23,6 +23,23 @@ const Player = () => {
           <img className="w-4 cursor-pointer" src={assets.next_icon} alt="" />
           <img className="w-4 cursor-pointer" src={assets.loop_icon} alt="" />
         </div>
+        <div className="flex items-center gap-5">
+          <p>1:10</p>
+          <div className="w-[60vw] max-w-[500px] bg-gray-500 rounded-full cursor-pointer">
+            <hr className="h-1 border-none w-0 bg-white rounded-full" />
+          </div>
+          <p>3:20</p>
+        </div>
+      </div>
+      <div className="hidden lg:flex items-center gap-2 opacity-75">
+        <img className="w-4" src={assets.plays_icon} alt="" />
+        <img className="w-4" src={assets.mic_icon} alt="" />
+        <img className="w-4" src={assets.queue_icon} alt="" />
+        <img className="w-4" src={assets.speaker_icon} alt="" />
+        <img className="w-4" src={assets.volume_icon} alt="" />
+        <div className="w-20 bg-slate-50 h-1 rounded"></div>
+        <img className="w-4" src={assets.mini_player_icon} alt="" />
+        <img className="w-4" src={assets.zoom_icon} alt="" />
       </div>
     </div>
   );
