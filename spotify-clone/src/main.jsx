96dcf368548c667fc8850e91f61ba80a8dd83c13
Kishan <kissanshaa@gmail.com>
2024-07-22 23:08:35 +0530

    import PlayerContextProvider

--- spotify-clone/src/main.jsx
+++ spotify-clone/src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import PlayerContextProvider from "./context/PlayerContext";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-    <BrowserRouter/>
+    <PlayerContextProvider>
+      <App />
+    </PlayerContextProvider>
+
+    <BrowserRouter />
   </React.StrictMode>,
   document.getElementById("root")
 );
