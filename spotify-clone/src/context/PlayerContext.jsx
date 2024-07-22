4d53d03c206a6c8824789aef1dc0ba80a4cc5253
Kishan <kissanshaa@gmail.com>
2024-07-22 23:10:23 +0530

    PlayerContext

---
+++ spotify-clone/src/context/PlayerContext.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { createContext, useRef } from "react";
+
+export const PlayerContext = createContext();
+
+const PlayerContextProvider = (props) => {
+  const audioRef = useRef();
+
+  const contextValue = {
+    audioRef,
+  };
+
+  return (
+    <PlayerContextProvider value={contextValue}>
+      {props.children}
+    </PlayerContextProvider>
+  );
+};
+
+export default PlayerContextProvider;
