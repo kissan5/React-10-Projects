4b0076c9853782eb1e1bfcd87ee1d8bac2800028
Kishan <kissanshaa@gmail.com>
2024-07-23 16:07:37 +0530

    track file added

--- spotify-clone/src/App.jsx
+++ spotify-clone/src/App.jsx
@@ -1,25 +1,22 @@
 import React, { useContext } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Player from "./components/Player";
 import Display from "./components/Display";
 import { PlayerContext } from "./context/PlayerContext";
 
 const App = () => {
-  const { audio } = useContext(PlayerContext);
+    const { audioRef,track } = useContext(PlayerContext);
 
-  return (
-    <Router>
-      <div className="h-screen bg-black">
-        <div className="h-[90%] flex">
-          <Sidebar />
-          <Display />
+    return (
+        <div className="h-screen bg-black">
+            <div className="h-[90%] flex">
+                <Sidebar />
+                <Display />
+            </div>
+            <Player />
+            <audio ref={audioRef} src={track.file} preload="auto"></audio>
         </div>
-        <Player />
-        <audio ref={audioRef} preload="auto"></audio>
-      </div>
-    </Router>
-  );
+    );
 };
 
 export default App;
