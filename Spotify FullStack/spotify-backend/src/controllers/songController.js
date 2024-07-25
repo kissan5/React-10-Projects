d1b333d20cad5ce646b8e9dedb9bc8831e07a117
Kishan <kissanshaa@gmail.com>
2024-07-26 00:21:47 +0530

    song data to remove and list them in database

--- Spotify FullStack/spotify-backend/src/controllers/songController.js
+++ Spotify FullStack/spotify-backend/src/controllers/songController.js
@@ -1,3 +1,6 @@
+import { v2 as cloudinary } from "cloudinary";
+import songModel from "../models/songModel.js";
+
 const addSong = async (req, res) => {
   try {
     const name = req.body.name;
@@ -5,19 +8,51 @@ const addSong = async (req, res) => {
     const album = req.body.album;
     const audioFile = req.files.audio[0];
     const imageFile = req.files.image[0];
-    const audioUpload = await connectCloudinary.uploader.upload(
-      audioFile.path,
-      { resoure_type: "video" }
-    );
-    const imageUpload = await connectCloudinary.uploader.upload(
-      imageFile.path,
-      { resource_type: "image" }
-    );
+    const audioUpload = await cloudinary.uploader.upload(audioFile.path, {
+      resource_type: "video",
+    });
+    const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
+      resource_type: "image",
+    });
 
-    console.log(name, desc, album, audioUpload, imageUpload);
-  } catch (error) {}
+    const duration = `${Math.floor(audioUpload.duration / 60)}:$${Math.floor(
+      audioUpload.duration % 60
+    )} `;
+
+    const songData = {
+      name,
+      desc,
+      album,
+      image: imageUpload.secure_url,
+      audio: audioUpload.secure_url,
+      duration,
+    };
+
+    const song = new songModel(songData);
+    await song.save();
+
+    res.json({ sucess: true, message: "Song Added" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
 };
 
-const listSong = async (req, res) => {};
+const listSong = async (req, res) => {
+  try {
+    const allSongs = await songModel.find({});
+    res.status(200).json({ success: true, songs: allSongs });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
 
-export { addSong, listSong };
+const removeSong = async (req, res) => {
+  try {
+    await songModel.findByIdAndDelete(req.body.id);
+    res.status(200).json({ success: true, message: "Song removed" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Error removing" });
+  }
+};
+
+export { addSong, listSong, removeSong };
