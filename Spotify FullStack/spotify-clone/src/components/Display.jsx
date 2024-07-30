bdf1621f4876459639d88e4f7850c98eafd09ed3
Kishan <kissanshaa@gmail.com>
2024-07-30 15:49:18 +0530

    useContext and player context added

--- Spotify FullStack/spotify-clone/src/components/Display.jsx
+++ Spotify FullStack/spotify-clone/src/components/Display.jsx
@@ -7,14 +7,20 @@ import {
 } from "react-router-dom";
 import DisplayHome from "./DisplayHome";
 import DisplayAlbum from "./DisplayAlbum";
-import { albumsData } from "../assets/assets";
+import { useContext } from "react";
+import { PlayerContext } from "../context/PlayerContext";
 
 const Display = () => {
+  const { albumsData } = useContext(PlayerContext);
+
   const displayRef = useRef();
   const location = useLocation();
   const isAlbum = location.pathname.includes("album");
-  const albumId = isAlbum ? location.pathname.slice(-1) : "";
-  const bgColor = albumsData[Number(albumId)].bgColor;
+  const albumId = isAlbum ? location.pathname.split("/").pop() : "";
+  const bgColor =
+    isAlbum && albumsData.length > 0
+      ? albumsData.find((x) => x._id === albumId)?.bgColour
+      : "#121212";
 
   useEffect(() => {
     if (isAlbum) {
@@ -22,17 +28,24 @@ const Display = () => {
     } else {
       displayRef.current.style.background = `#121212`;
     }
-  });
+  }, [isAlbum, bgColor]);
 
   return (
     <div
       ref={displayRef}
       className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0"
     >
-      <Routes>
-        <Route path="/" element={<DisplayHome />} />
-        <Route path="/album/:id" element={<DisplayAlbum />} />
-      </Routes>
+      {albumsData.length > 0 ? (
+        <Routes>
+          <Route path="/" element={<DisplayHome />} />
+          <Route
+            path="/album/:id"
+            element={
+              <DisplayAlbum album={albumsData.find((x) => x._id == albumId)} />
+            }
+          />
+        </Routes>
+      ) : null}
     </div>
   );
 };
