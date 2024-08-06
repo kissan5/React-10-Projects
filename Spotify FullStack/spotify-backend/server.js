429c6ac61e7844a4bb36bbf9b92e6bd3937f0a9f
Kishan <kissanshaa@gmail.com>
2024-08-06 14:22:19 +0530

    packages

--- Spotify FullStack/spotify-backend/server.js
+++ Spotify FullStack/spotify-backend/server.js
@@ -5,6 +5,7 @@ import songRouter from "./src/routes/songRoute.js";
 import connectDB from "./src/config/mongodb.js";
 import configCloudinary from "./src/config/configCloudinary.js";
 import albumRouter from "./src/routes/albumRoute.js";
+// import authRouter from "./src/routes/authRoute.js";
 
 //app config
 const app = express();
@@ -18,6 +19,7 @@ app.use(cors());
 //initializing routes
 app.use("/api/song", songRouter);
 app.use("/api/album", albumRouter)
+// app.use("/api/auth", authRouter)
 
 app.get("/", (req, res) => res.send("API Working"));
 
