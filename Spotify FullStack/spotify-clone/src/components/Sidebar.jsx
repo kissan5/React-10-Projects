c12ad11c3a1b263e6980a666d3ec9c3cfb671625
Kishan <kissanshaa@gmail.com>
2024-08-06 14:30:33 +0530

    navigate playlist

--- Spotify FullStack/spotify-clone/src/components/Sidebar.jsx
+++ Spotify FullStack/spotify-clone/src/components/Sidebar.jsx
@@ -5,6 +5,10 @@ import { useNavigate } from "react-router-dom";
 const Sidebar = () => {
   const navigate = useNavigate();
 
+  const handleCreatePlaylist = () => {
+    navigate("/create-playlist");
+  };
+
   return (
     <div className="w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex">
       <div className="bg-[#121212] h-[15%] rounded flex flex-col justify-around">
@@ -29,13 +33,18 @@ const Sidebar = () => {
           </div>
           <div className="flex item-center gap-3">
             <img className="w-5" src={assets.arrow_icon} alt="" />
-            <img className="w-5" src={assets.plus_icon} alt="" />
+            <button onClick={handleCreatePlaylist}>
+              <img className="w-5" src={assets.plus_icon} alt="" />
+            </button>
           </div>
         </div>
         <div className="p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4">
           <h1>Create your first playlist</h1>
           <p>it's easy we will help you</p>
-          <button className="px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4">
+          <button
+            onClick={handleCreatePlaylist}
+            className="px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4"
+          >
             Create Playlist
           </button>
         </div>
