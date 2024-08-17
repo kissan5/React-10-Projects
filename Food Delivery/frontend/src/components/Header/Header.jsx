ca7759eba9b6199c886d87398236714374fe784f
Kishan <kissanshaa@gmail.com>
2024-08-18 01:05:30 +0530

    header for the below content like menu and foods which you want to order

--- Food Delivery/frontend/src/components/Header/Header.jsx
+++ Food Delivery/frontend/src/components/Header/Header.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import './Header.css'
+import React from "react";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className="header">
+      <div className="header-contents">
+        <h2>Order Your favourite food here</h2>
+        <p>
+          Explore our diverse menu brimming with mouthwatering dishes, each
+          crafted from the finest ingredients with culinary finesse. We're
+          dedicated to delighting your taste buds and transforming every meal
+          into a memorable dining adventure.
+        </p>
+        <button>View Menu</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
