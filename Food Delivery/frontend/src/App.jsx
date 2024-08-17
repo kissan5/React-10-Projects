823c8ce2a068401425474b27ae4b306bdf2b7539
Kishan <kissanshaa@gmail.com>
2024-08-18 01:02:27 +0530

    imported routes route

--- Food Delivery/frontend/src/App.jsx
+++ Food Delivery/frontend/src/App.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Cart from './pages/Cart/Cart';
-import Home from './pages/Home/Home';
-import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Cart from "./pages/Cart/Cart";
+import Home from "./pages/Home/Home";
+import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 
 const App = () => {
   return (
-    <div className='app'>
-      <Navbar/>
+    <div className="app">
+      <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/cart' element={<Cart />} />
-        <Route path='/order' element={<PlaceOrder />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/order" element={<PlaceOrder />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
