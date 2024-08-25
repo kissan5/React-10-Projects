b0845599e30c4e5d729643992bbcb9ada4d84d3b
Kishan <kissanshaa@gmail.com>
2024-08-25 15:43:37 +0530

    footer added

---
+++ Food Delivery/frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "./Footer.css";
+import { assets } from "../../assets/assets";
+
+const Footer = () => {
+  return (
+    <div className="footer" id="fopter">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
+            voluptates fuga quibusdam, maiores magni nam unde nulla, vel quidem
+            aliquid totam voluptate quisquam, voluptatibus voluptatum. Totam,
+            magni. Libero itaque totam aut fugiat hic accusamus odit deserunt
+            vitae, ullam veniam exercitationem quod rem, autem reprehenderit
+            dignissimos? Consectetur esse impedit fuga aliquid?
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+91 8210951464</li>
+            <li>Contct @tomato.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright 2024 © Tomato.com - All Right Reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
