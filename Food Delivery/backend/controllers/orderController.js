2749286c57d13efe2c9672107d62173332b07e11
Kishan <kissanshaa@gmail.com>
2024-10-21 20:10:09 +0530

    added listing order for admin panel and api for updating status

--- Food Delivery/backend/controllers/orderController.js
+++ Food Delivery/backend/controllers/orderController.js
@@ -82,4 +82,28 @@ const userOrder = async (req, res) => {
   }
 };
 
-export { placeOrder, verifyOrder, userOrder };
+// Listing orders for admin panel
+const listOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({});
+    res.json({ success: true, data: orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+// api for updating status
+const updateStatus = async (req, res) => {
+  try {
+    await orderModel.findByIdAndUpdate(req.body.orderId, {
+      status: req.body.status,
+    });
+    res.json({ success: true, message: "Status Updated" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+export { placeOrder, verifyOrder, userOrder, listOrders, updateStatus };
