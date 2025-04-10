6d2a575f6b500b3f30755bca7d4979ea79ec6fcf
Kishan <kissanshaa@gmail.com>
2025-04-10 09:44:49 +0530

    Delete Food Delivery - Copy directory

--- Food Delivery - Copy/backend/config/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-  await mongoose
-    .connect(
-      "mongodb+srv://hitclad:mancity@cluster0.kjy9qoj.mongodb.net/food-delivery"
-    )
-    .then(() => console.log("DB Connected"));
-};
