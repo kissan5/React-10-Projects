883eb2d58fe6b17b1899dc14cb4b45b28ad20270
Kishan <kissanshaa@gmail.com>
2025-04-21 15:51:51 +0530

    footer updated with name and other link

--- Food Delivery/frontend/src/components/Footer/Footer.jsx
+++ Food Delivery/frontend/src/components/Footer/Footer.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import "./Footer.css";
 import { assets } from "../../assets/assets";
+import { Link } from "react-router-dom"; // ✅ import Link
 
 const Footer = () => {
   return (
-    <div className="footer" id="fopter">
+    <div className="footer" id="footer">
       <div className="footer-content">
         <div className="footer-content-left">
           <img src={assets.logo} alt="" />
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
-            voluptates fuga quibusdam, maiores magni nam unde nulla, vel quidem
-            aliquid totam voluptate quisquam, voluptatibus voluptatum. Totam,
-            magni. Libero itaque totam aut fugiat hic accusamus odit deserunt
-            vitae, ullam veniam exercitationem quod rem, autem reprehenderit
-            dignissimos? Consectetur esse impedit fuga aliquid?
+            At FoodDel, we are committed to delivering your favorite meals from
+            the best local restaurants straight to your doorstep. With a focus
+            on quality, speed, and convenience, we make sure your cravings are
+            satisfied effortlessly. From easy ordering to real-time tracking,
+            enjoy a seamless food delivery experience every time.
           </p>
           <div className="footer-social-icons">
             <img src={assets.facebook_icon} alt="" />
@@ -22,26 +22,37 @@ const Footer = () => {
             <img src={assets.linkedin_icon} alt="" />
           </div>
         </div>
+
         <div className="footer-content-center">
           <h2>COMPANY</h2>
           <ul>
-            <li>Home</li>
-            <li>About us</li>
-            <li>Delivery</li>
-            <li>Privacy Policy</li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about-us">About Us</Link>
+            </li>
+            <li>
+              <Link to="/delivery">Delivery</Link> 
+            </li>
+            <li>
+              <Link to="/privacy-policy">Privacy Policy</Link> 
+            </li>
           </ul>
         </div>
+
         <div className="footer-content-right">
           <h2>GET IN TOUCH</h2>
           <ul>
-            <li>+91 8210951464</li>
-            <li>Contct @tomato.com</li>
+            <li>+91 12345 67890</li>
+            <li>Contact@foodDel</li>
           </ul>
         </div>
       </div>
+
       <hr />
       <p className="footer-copyright">
-        Copyright 2024 © Tomato.com - All Right Reserved.
+        Copyright 2025 © FoodDel - All Right Reserved.
       </p>
     </div>
   );
